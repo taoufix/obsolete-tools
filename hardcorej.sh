5a37f385a30ab28530609320c8424f3f349c4b80
taoufik <taoufik@tao.localdomain>
2009-05-12 00:09:00 +0100

    Deleted hardcorej.sh

--- hardcorej.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mplayer "http://www.live365.com/play/42200?auth=a4d99c79f3960614dcedc31240e1758a-1196184655-rec4&ss=foobarox%3ARE59JCcHE4Myd1U5&tag=live365&token=bc262d5fa7618c6f0db86d966fb541a2-3009270081000000&sid=85.124.83.2-1196085886812385&lid=9-aut&from=pls"
