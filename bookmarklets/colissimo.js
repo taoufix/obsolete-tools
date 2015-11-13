a6779b610806766493bb84d1ca15448f824252a0
Taoufik El Aoumari <taoufix@gmail.com>
2015-11-13 09:32:20 +0100

    Mimified.

--- bookmarklets/colissimo.js
+++ bookmarklets/colissimo.js
@@ -1,21 +1,10 @@
-function post(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-         }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-}
-post('http://www.colissimo.fr/portail_colissimo/suivreResultat.do', {parcelnumber: 'XXX'});
+javascript:form=document.createElement("form");
+form.setAttribute("method","post");
+form.setAttribute("action",'http://www.colissimo.fr/portail_colissimo/suivreResultat.do');
+hiddenField=document.createElement("input");
+hiddenField.setAttribute("type","hidden");
+hiddenField.setAttribute("name",'parcelnumber');
+hiddenField.setAttribute("value",'XXXX');
+form.appendChild(hiddenField);
+document.body.appendChild(form);
+form.submit();
